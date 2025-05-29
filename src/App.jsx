92c439bcfcd5b540b7bb47bfefd9e4aa05ee16b6
Muhamed Razik <muhammedrazik987@gmail.com>
2025-05-29 19:53:11 +0530

    feat: integrate shadcn/ui and apply custom teal-based theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Button } from "@/components/ui/button"
+
 
 const App = () => {
   return (
     <div className='m-5 p-3'>
       <h1 className='text-red-500 bg-teal-300 text-center font-mono font-extrabold m-2 p-3'>Student Sync</h1>
+      <Button variant="default">Button</Button>
     </div>
   )
 }
