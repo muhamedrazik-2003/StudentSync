37b3de1a46c77113670943c6e15834eb9db35ca6
Muhamed Razik <muhammedrazik987@gmail.com>
2025-05-29 17:19:10 +0530

    chore: initialize Vite + React with Tailwind, add logos, clean up default files

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='m-5 p-3'>
+      <h1 className='text-red-500 bg-teal-300 text-center font-mono font-extrabold m-2 p-3'>Student Sync</h1>
+    </div>
+  )
+}
+
+export default App
