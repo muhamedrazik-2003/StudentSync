c90ad6da94e8100b1cb0ccc322e4d08ba677382b
Muhamed Razik <muhammedrazik987@gmail.com>
2025-06-06 01:29:49 +0530

    fix : login background image not showing fixed

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import StudentListPage from './pages/StudentsPage'
 import Grades from './pages/Grades'
 import Courses from './pages/Courses'
 import SettingsPage from './pages/SettingsPage'
-import { Routes, Route,useLocation } from 'react-router-dom'
+import { Routes, Route, useLocation } from 'react-router-dom'
 import Login from './pages/Login'
 
 const App = () => {
@@ -13,20 +13,28 @@ const App = () => {
   const isLoginPage = location.pathname === '/';
 
   return isLoginPage ? (
-    <main className='bg-[url(loginBg.png)] bg-cover bg-center min-h-screen h-[100vh] grid place-content-center'>
+    <main
+      style={{
+        backgroundImage: "url('../public/loginbg.png')",
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        height: '100vh',
+      }}
+      className='min-h-screen h-[100vh] grid place-content-center'
+    >
       <Routes>
-        <Route path='/' element={<Login/>}/>
+        <Route path='/' element={<Login />} />
       </Routes>
     </main>
   ) : (
     <main className='grid grid-cols-[50px_1fr] md:grid-cols-[220px_1fr] bg-sidebar text-sidebar-foreground  p-1 md:p-3'>
-      <Sidebar/>
+      <Sidebar />
       <Routes>
-        <Route path='/dashboard' element={<Dashboard/>}/>
-        <Route path='/students' element={<StudentListPage/>}/>
-        <Route path='/courses' element={<Courses/>}/>
-        <Route path='/grades' element={<Grades/>}/>
-        <Route path='/settings' element={<SettingsPage/>}/>
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/students' element={<StudentListPage />} />
+        <Route path='/courses' element={<Courses />} />
+        <Route path='/grades' element={<Grades />} />
+        <Route path='/settings' element={<SettingsPage />} />
       </Routes>
     </main>
   )
